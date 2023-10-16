cac488ed35ca12fc008602658a020d1a906a7cf2
Evgeniy Berezin <79792471+FatherRtd@users.noreply.github.com>
2023-10-16 19:32:29 +0500

    Контейнеризация фронта (#4)

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,18 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-const HOST = "0.0.0.0";
+// const HOST = "0.0.0.0";
 const api = "https://localhost:8000";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: HOST,
+    watch: {
+      usePolling: true
+    },
+    host: true,
+    strictPort: true,
     port: 4200,
     proxy: {
       "^/api": {
